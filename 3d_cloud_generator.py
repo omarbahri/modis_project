7e31dfeef4a2673c9df31e7dd960c4e8b57a0cfb
Brent Buffenbarger <brent.buffenbarger@gmail.com>
2021-04-22 14:10:26 -0600

    Script now generates 9 clouds types - also generates rain and ice clouds

--- 3d_cloud_generator.py
+++ 3d_cloud_generator.py
@@ -1,39 +1,93 @@
 import numpy as np
-from pyevtk.hl import gridToVTK
+from tvtk.api import tvtk, write_data
 from PIL import Image
 import os
 import time
+import random
+import math
+
+def classify_cloud(opt_thickness, pressure):
+    if 50 <= pressure < 440:  # high elevation
+        if 0 <= opt_thickness < 3.6:
+            return 'cumulus'
+        elif 3.6 <= opt_thickness < 23:
+            return 'stratocumulus'
+        elif 23 <= opt_thickness:
+            return 'stratus'
+    elif 440 <= pressure < 680:  # mid elevation
+        if 0 <= opt_thickness < 3.6:
+            return 'altocumulus'
+        elif 3.6 <= opt_thickness < 23:
+            return 'altostratus'
+        elif 23 <= opt_thickness:
+            return 'nimbostratus'
+    elif 680 <= pressure:  # low elevation
+        if 0 <= opt_thickness < 3.6:
+            return 'cirrus'
+        elif 3.6 <= opt_thickness < 23:
+            return 'cirrostratus'
+        elif 23 <= opt_thickness:
+            return 'deep_convection'
+
+    return 'unknown'
+
+# dictionary storing the cloud type and average total height
+cloud_types_height = {
+    'cumulus': [1, 1750],
+    'stratocumulus': [2, 300],
+    'stratus': [3, 750],
+    'altocumulus': [4, 450],
+    'altostratus': [5, 1100],
+    'nimbostratus': [6, 750],
+    'cirrus': [7, 300],
+    'cirrostratus': [8, 300],
+    'deep_convection': [9, 2000],
+    'unknown': [0, 0]
+}
 
 directory = "./Data/"
-file_name = "cloud_height"
-file_extension = ".vtr"
+height_file_name = "cloud_height"
+rain_file_name = "rain_cloud"
+ice_file_name = "ice_cloud"
+file_extension = ".vti"
 
-# delete the data file if it already exists
+# delete the data files if they already exists
 for file in os.listdir(directory):
-    if file.startswith(file_name) and file.endswith(file_extension):
+    if file.endswith(file_extension):
         os.remove(os.path.join(directory, file))
 
-# load the .tif files into a numpy array
+# load the .tif files into numpy arrays
+# cloud top height
 CTH_tif = Image.open(f'{directory}/mod06_CTH.tif')
 CTH_arr = np.array(CTH_tif, dtype='uint16')
 
-# Initialize the ranges of the cloud elevations
-low_elev = (500 // 500, 2000 // 500)
-mid_elev = ((2000 // 500) + 1, 7000 // 500)
-high_elev = ((7000 // 500) + 1, np.amax(CTH_arr) // 500)
+# cloud optical thickness
+COT_tif = Image.open(f'{directory}/mod06_COT.tif')
+COT_arr = np.array(COT_tif, dtype='uint16')
+COT_scale_factor = 0.00999999977648258
 
-# Initialize the pixel values for each cloud type
-low_cloud = 1
-mid_cloud = 2
-high_cloud = 3
+# cloud top pressure
+CTP_tif = Image.open(f'{directory}/mod06_CTP.tif')
+CTP_arr = np.array(CTP_tif, dtype='uint16')
+CTP_scale_factor = 0.100000001490116
+
+# cloud phase optical properties
+CPOP_tif = Image.open(f'{directory}/mod06_CPOP.tif')
+CPOP_arr = np.array(CPOP_tif, dtype='uint16')
+
+# Initialize the ranges of the cloud elevations
+res_factor = 50
+low_elev = (500 // res_factor, 2000 // res_factor)
+mid_elev = ((2000 // res_factor) + 1, 7000 // res_factor)
+high_elev = ((7000 // res_factor) + 1, np.amax(CTH_arr) // res_factor)
 
 # Get the x, y, and z dimensions
-x_shrink_factor = 2
-y_shrink_factor = 2
+x_shrink_factor = 1
+y_shrink_factor = 1
 z_shrink_factor = 1
 x_dim = np.shape(CTH_arr)[0] // x_shrink_factor
 y_dim = np.shape(CTH_arr)[1] // y_shrink_factor
-z_dim = (np.amax(CTH_arr) // 500) // z_shrink_factor
+z_dim = (math.ceil((np.amax(CTH_arr) * 1.025)) // res_factor) // z_shrink_factor
 print(f"Minimum Elevation: {np.amin(CTH_arr)}")
 print(f"Maximum Elevation: {np.amax(CTH_arr)}")
 print(f"Dimensions: ({x_dim}, {y_dim}, {z_dim})")
@@ -45,6 +99,11 @@ file_counter = 0
 while row < x_shrink_factor and col < y_shrink_factor and height < z_shrink_factor:
     # Initialize the 3D array that will hold the cloud data
     cloud_height_arr = np.zeros((y_dim, x_dim, z_dim), dtype='uint8')
+    cloud_height_arr.fill(np.nan)
+    ice_cloud_arr = np.zeros((y_dim, x_dim, z_dim), dtype='uint8')
+    ice_cloud_arr.fill(np.nan)
+    rain_cloud_arr = np.zeros((y_dim, x_dim, z_dim), dtype='uint8')
+    rain_cloud_arr.fill(np.nan)
 
     start_x = col * x_dim
     start_y = row * y_dim
@@ -54,30 +113,75 @@ while row < x_shrink_factor and col < y_shrink_factor and height < z_shrink_fact
         if y % 100 == 0:
             print(f"Progress: {y} / {y_dim} (Updates every 100 iterations)")
         for x in range(x_dim):
-            for z in range(z_dim):
-                pixel_value = CTH_arr[start_x + x][start_y + y] // 500
-                if low_elev[0] <= pixel_value <= low_elev[1] and low_elev[0] <= z <= low_elev[1]:
-                    cloud_height_arr[y][x][z] = low_cloud
-                if mid_elev[0] <= pixel_value // 500 <= mid_elev[1] and mid_elev[0] <= z <= mid_elev[1]:
-                    cloud_height_arr[y][x][z] = mid_cloud
-                if high_elev[0] <= pixel_value <= high_elev[1] and high_elev[0] <= z <= high_elev[1]:
-                    cloud_height_arr[y][x][z] = high_cloud
-                else:
-                    cloud_height_arr[y][x][z] = 0
+            top_height = CTH_arr[start_x + x][start_y + y] // res_factor
+            opt_thickness = COT_arr[start_x + x][start_y + y] * COT_scale_factor
+            pressure = CTP_arr[start_x + x][start_y + y] * CTP_scale_factor
 
-    # Write the data to a new file
-    x = np.arange(x_dim + 1)
-    y = np.arange(y_dim + 1)
-    z = np.arange(z_dim + 1,)
+            type_and_base = cloud_types_height[classify_cloud(opt_thickness, pressure)]
+            cloud_type = type_and_base[0]
+            base_height = top_height - (type_and_base[1] // res_factor)
 
+            # We can randomly vary the base_height to get more realistic clouds
+            rand_offset = random.uniform(-0.2, 0.2)
+            base_height *= (1 + rand_offset)
+            base_height = math.ceil(base_height)
+
+            # push the clouds up that have a negative base_height
+            if base_height < 0:
+                z_transform = (-1 * base_height)
+                base_height += z_transform
+                top_height += z_transform
+
+            cloud_height_arr[y][x][base_height:top_height] = cloud_type
+
+            # decide if the cloud is liquid or ice
+            if CPOP_arr[start_x + x][start_y + y] == 2:
+                rain_cloud_arr[y][x][base_height:top_height] = 2
+            elif CPOP_arr[start_x + x][start_y + y] == 3:
+                ice_cloud_arr[y][x][base_height:top_height] = 3
+
+    # flip the array upside down so it renders right side up in Paraview
+    np.flipud(cloud_height_arr)
+    np.flipud(rain_cloud_arr)
+    np.flipud(ice_cloud_arr)
+
+    # Basic logging information
     print(f"Shape: {np.shape(cloud_height_arr)}")
     print(f"Size of array in MB: {(cloud_height_arr.size * cloud_height_arr.itemsize) // 1000000}")
     end = time.time()
     print(f"Time taken to compute {end - start}")
 
-    print(f"Writing cloud data to file {file_name}{file_counter}.vtr")
-    gridToVTK(f"./Data/{file_name}{file_counter}", y, x, z, cellData={'cloud_type': cloud_height_arr})
-    print(f"Finished writing to {file_name}{file_counter}.vtr")
+    # Write the data to files
+    print(f"Writing cloud height data to file {height_file_name}{file_counter}{file_extension}")
+    cloud_height_data = tvtk.ImageData(spacing=(1, 1, 1), origin=(0, 0, 0), dimensions=cloud_height_arr.shape)
+    cloud_height_data.point_data.scalars = cloud_height_arr.ravel(order='F')
+    cloud_height_data.point_data.scalars.name = 'Cloud_Heights'
+    write_data(cloud_height_data, f"./Data/{height_file_name}{file_counter}")
+    print(f"Finished writing to {height_file_name}{file_counter}{file_extension}")
+    if x_shrink_factor == 1 and y_shrink_factor == 1 and z_shrink_factor == 1:
+        del cloud_height_arr
+        del cloud_height_data
+
+    print(f"Writing rain cloud data to file {rain_file_name}{file_counter}{file_extension}")
+    rain_cloud_data = tvtk.ImageData(spacing=(1, 1, 1), origin=(0, 0, 0), dimensions=rain_cloud_arr.shape)
+    rain_cloud_data.point_data.scalars = rain_cloud_arr.ravel(order='F')
+    rain_cloud_data.point_data.scalars.name = 'Cloud_Heights'
+    write_data(rain_cloud_data, f"./Data/{rain_file_name}{file_counter}")
+    print(f"Finished writing to {rain_file_name}{file_counter}{file_extension}")
+    if x_shrink_factor == 1 and y_shrink_factor == 1 and z_shrink_factor == 1:
+        del rain_cloud_arr
+        del rain_cloud_data
+
+    print(f"Writing ice cloud data to file {ice_file_name}{file_counter}{file_extension}")
+    ice_cloud_data = tvtk.ImageData(spacing=(1, 1, 1), origin=(0, 0, 0), dimensions=ice_cloud_arr.shape)
+    ice_cloud_data.point_data.scalars = ice_cloud_arr.ravel(order='F')
+    ice_cloud_data.point_data.scalars.name = 'Cloud_Heights'
+    write_data(ice_cloud_data, f"./Data/{ice_file_name}{file_counter}")
+    print(f"Finished writing to {ice_file_name}{file_counter}{file_extension}")
+    if x_shrink_factor == 1 and y_shrink_factor == 1 and z_shrink_factor == 1:
+        del ice_cloud_arr
+        del ice_cloud_data
+
     file_counter += 1
     print(f"{(x_shrink_factor * y_shrink_factor * z_shrink_factor) -  file_counter} files to go.")
 
